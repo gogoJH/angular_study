80c06c6fefb3f3561174289ec54f992d5900d076
jinjo <jinjo@pocl.io>
2020-05-08 09:29:24 +0900

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { HEROES } from './../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
